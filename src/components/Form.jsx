9931938e8fbb8d0055d4cd0369efdfba3420d5da
Austin Obnillas <austinobnillas@gmail.com>
2023-06-12 15:20:07 -0700

    Hook Form Assignment Completed

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -33,11 +33,11 @@ const Form = (props) => {
             </form>
             <div>
                 <h3>Your Form Data:</h3>
-                <p>{firstName}</p>
-                <p>{lastName}</p>
-                <p>{email}</p>
-                <p>{password}</p>
-                <p>{passwordConfirm}</p>
+                <p>First Name: {firstName}</p>
+                <p>Last Name: {lastName}</p>
+                <p>Email: {email}</p>
+                <p>Password: {password}</p>
+                <p>Confirmed Password: {passwordConfirm}</p>
             </div>
         </div>
     );
