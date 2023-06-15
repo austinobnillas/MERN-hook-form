cc613a0c051cf0471909c236345cfc814a31bd96
Austin Obnillas <austinobnillas@gmail.com>
2023-06-15 15:16:55 -0700

    Completed More Forms Assignment

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -13,22 +13,37 @@ const Form = (props) => {
                 <div>
                     <label>First Name: </label>
                     <input type="text" value={firstName} onChange={ (e) => setFirstName(e.target.value) }></input>
+                    {firstName.length < 2 && firstName.length > 0 ? (
+                        <p>First Name must be at least 2 characters long</p>
+                    ) : "" }
                 </div>
                 <div>
                     <label>Last Name: </label>
                     <input type="text" value={lastName} onChange={ (e) => setLastName(e.target.value) }></input>
+                    {lastName.length < 2 && lastName.length > 0 ? (
+                        <p>Last Name must be at least 2 characters long</p>
+                    ) : "" }
                 </div>
                 <div>
                     <label>Email: </label>
                     <input type="text" value={email} onChange={ (e) => setEmail(e.target.value) }></input>
+                    {email.length < 5 && email.length > 0 ? (
+                        <p>Email must be at least 5 characters long</p>
+                    ) : "" }
                 </div>
                 <div>
                     <label>Password: </label>
                     <input type="password" value={password} onChange={ (e) => setPassword(e.target.value) }></input>
+                    {password.length < 8 && password.length > 0 ? (
+                        <p>Password must be at least 8 characters long</p>
+                    ) : "" }
                 </div>
                 <div>
                     <label>Confirm Password: </label>
                     <input type="password" value={passwordConfirm} onChange={ (e) => setPasswordConfirm(e.target.value)}></input>
+                    {password !== passwordConfirm ? (
+                        <p>Passwords must match</p>
+                    ) : "" }
                 </div>
             </form>
             <div>
